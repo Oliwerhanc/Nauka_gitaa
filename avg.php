2e4b17d0800cb6db69c8b44f0332049030b854ac
Oliwerhanc <kolinirp@gmail.com>
2025-04-25 16:30:48 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -3,3 +3,15 @@ function srednia($numbers) {
     return array_sum($numbers) / count($numbers);
 }
 ?>
+
+<?php
+function srednia_wazona($numbers, $weights) {
+    $sum = 0;
+    $weight_sum = 0;
+    foreach ($numbers as $index => $number) {
+        $sum += $number * $weights[$index];
+        $weight_sum += $weights[$index];
+    }
+    return $sum / $weight_sum;
+}
+?>
